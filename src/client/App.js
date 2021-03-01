57bf1581f79a4c6f2594f5f203acd1cca0305810
Symon Lopes <symon.lopes@nwaypro.com>
2021-03-01 15:37:23 -0300

    Configurando WebPack

---
+++ src/client/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello, World 2! </h1>
+        <button onClick={() => {
+          console.log('hauehiuahuehauihuiehaiuheiu', new Date().getTime())
+        }}>Log Something 2...</button>
+      </div>
+    );
+  }
+}
+
+export default App;
